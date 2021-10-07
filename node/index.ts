8ab6070e9b80e7e7806bdd1c75f6ccd6267f757c
Paulalex85 <tessier.paulalexandre@orange.fr>
2021-10-07 17:05:43 +0200

    listening new blocks

--- node/index.ts
+++ node/index.ts
@@ -1,7 +1,19 @@
-const http = require('http');
+import { ethers } from 'ethers';
 
-http.createServer(() => {
+const net = require('net');
+const INFURA_ID = 'c668ee6214a74e1c89726b345a5aed66';
+const network = 'homestead';
 
+const provider = ethers.getDefaultProvider(network, {
+    infura: INFURA_ID,
+});
+
+net.createServer(() => {
 }).listen(3000, function() {
     console.log('server start at port 3000'); //the server object listens on port 3000
+    provider.on('block', (blockNumber: number) => {
+        provider.getBlock(blockNumber).then((block: ethers.providers.Block) => {
+            console.log(block);
+        });
+    });
 });
